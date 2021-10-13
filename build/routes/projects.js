33b6b623c7f6df726b6c42a8bce384a82d120508
William W <wille0403@gmail.com>
2021-10-13 22:08:30 +0000

    fix: Remove build folder

--- build/routes/projects.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { default: Project } = require('../controllers/Project');
-const authenticateJWT = require('../services/authenticateJWT');
-const router = express.Router();
-router.get('/', authenticateJWT, Project.getAll);
-module.exports = router;
--- build/services/getTechnique.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getTechnique = (project_id, technique_id) => {
-};
-exports.default = getTechnique;
